863df2909355212caf7d0a6aa0d8fb742dd75363
Essence Roberts <essencenroberts@gmail.com>
2026-08-17 11:18:30 -0500

    created and organized folder s and connected vite , tailwindcss, and typescript successfully

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-red-500 text-white">
+    <h1 class="text-4xl font-extrabold drop-shadow">Rest Countries Api</h1>
+    <p class="mt-4 text-slate-200"> Today we are creating the rest countries api project</p>
+  </div>
+</section> `;
